02ed74f1b68e1f16b72d3c0821eeaf4e1bcf66ac
dangnhat39 <nhathat111@gmail.com>
2025-03-03 08:17:52 +0700

    update

--- session18/baitap/baitap2.js
+++ session18/baitap/baitap2.js
@@ -2,7 +2,7 @@ let math = Number(prompt("Mời bạn nhập điểm toán"));
 let literature = Number(prompt("Mời bạn nhập điểm văn"));
 let eng = Number(prompt("Mời bạn nhập điểm tiếng Anh"));
 let tb = (math + eng + literature) / 3;
-if (tb >= 8) {
+if (tb >= 8 && tb <= 10) {
     document.write("bạn được học sinh giỏi");
 
 } else if (tb > 6.5 && tb < 7.9) {
