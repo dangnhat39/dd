8c36b1d9d6698fe5680be4bf412f09ffd900b34d
dangnhat39 <nhathat111@gmail.com>
2025-03-19 22:14:21 +0700

    update

---
+++ session31/baitap8.js
@@ -0,0 +1,18 @@
+
+document.querySelector('.addBtn').addEventListener('click', function () {
+    const inputValue = document.getElementById('myInput').value;
+    if (inputValue === '') {
+        alert('Vui lòng nhập một công việc!');
+        return;
+    }
+    const li = document.createElement('li');
+    li.textContent = inputValue;
+    
+    document.getElementById('myUL').appendChild(li);
+    document.getElementById('myInput').value = '';
+});
+document.getElementById('myUL').addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('checked');
+    }
+});
