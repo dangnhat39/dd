707e0908e278c6389785919e9094f86777eb4a2f
dangnhat39 <nhathat111@gmail.com>
2025-04-01 23:05:58 +0700

    update

--- test/test.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('registrationForm').addEventListener('submit', function (event)){
-    event.preventDefault();
-    let email = document.getElementById('email').ariaValueMax.trim();
-    let password = document.getElementById('password').value;
-    let confirmPassword =document.getElementById('confir')
-}
