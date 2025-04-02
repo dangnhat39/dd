ab823a72efa27f12451c1802b9dc6ae9b4ee80db
dangnhat39 <nhathat111@gmail.com>
2025-04-03 00:55:16 +0700

    update

--- project_dangnhat/js/dashboard.js
+++ project_dangnhat/js/dashboard.js
@@ -1,18 +1,54 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-    const items = document.getElementsByClassName("category-menu");
-    for (let i = 0; i < items.length; i++) {
-        items[i].addEventListener("click", function (event) {
-            event.preventDefault();
-            window.location.href = "/project_dangnhat/pages/dashboard.html";
-        });
-    }
+    const items = document.getElementById("category-menu");
+    items.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "/project_dangnhat/pages/dashboard.html";
+    });
 
-    const itemss = document.getElementsByClassName("category-pro");
-    for (let i = 0; i < itemss.length; i++) {
-        itemss[i].addEventListener("click", function (event) {
-            event.preventDefault();
-            window.location.href = "/project_dangnhat/pages/product.html";
-        });
-    }
+    const itemss = document.getElementById("category-pro");
+    itemss.addEventListener("click", function (event) {
+        event.preventDefault();
+        window.location.href = "/project_dangnhat/pages/login.html";
+    });
 });
+const modal = document.querySelector(".modal"); // Selects the modal by class  
+const openModal = document.getElementById("myModal");// Selects the button to open the modal  
+const cancelButton = document.getElementById("cancelButton");
+
+
+openModal.addEventListener("click", function () {
+    modal.style.display = "flex";
+});
+
+cancelButton.addEventListener("click", function () {
+    modal.style.display = "none";
+});
+
+
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+const editModal = document.querySelector(".edit-modal");
+const openEditModals = document.getElementsByClassName("edit-button");
+const cancelButtonEditModal = document.getElementsByClassName("cancel-button");
+
+for (let i = 0; i < openEditModals.length; i++) {
+    openEditModals[i].addEventListener("click", function () {
+        editModal.style.display = "flex";
+    });
+}
+
+for (let i = 0; i < cancelButtonEditModal.length; i++) {
+    cancelButtonEditModal[i].addEventListener("click", function () {
+        editModal.style.display = "none";
+    });
+}
+
+window.onclick = function (event) {
+    if (event.target === editModal) {
+        editModal.style.display = "none";
+    }
+}  
---
+++ test/test.js
@@ -0,0 +1,9 @@
+function cancel() {
+    // Logic to close the modal  
+    console.log("Modal closed");
+}
+
+function add() {
+    // Logic to handle adding the category  
+    console.log("Category added");
+}  
