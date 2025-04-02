f4bdf9d0e1fb66e973399e576e515209df2038d7
dangnhat39 <nhathat111@gmail.com>
2025-04-02 11:46:46 +0700

    update

--- project_dangnhat/js/dashboard.js
+++ project_dangnhat/js/dashboard.js
@@ -1,8 +1,18 @@
-const items = document.getElementsByClassName("category-menu");
 
-for (let i = 0; i < items.length; i++) {
-    items[i].addEventListener("click", function (event) {
-        event.preventDefault();
-        window.location.href = "/project_dangnhat/pages/dashboard.html";
-    });
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const items = document.getElementsByClassName("category-menu");
+    for (let i = 0; i < items.length; i++) {
+        items[i].addEventListener("click", function (event) {
+            event.preventDefault();
+            window.location.href = "/project_dangnhat/pages/dashboard.html";
+        });
+    }
+
+    const itemss = document.getElementsByClassName("category-pro");
+    for (let i = 0; i < itemss.length; i++) {
+        itemss[i].addEventListener("click", function (event) {
+            event.preventDefault();
+            window.location.href = "/project_dangnhat/pages/product.html";
+        });
+    }
+});
