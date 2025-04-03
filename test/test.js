0e6966f371d06e990bae2a936a73103836ba21da
dangnhat39 <nhathat111@gmail.com>
2025-04-03 08:13:41 +0700

    update

--- test/test.js
+++
@@ -1,9 +0,0 @@
-function cancel() {
-    // Logic to close the modal  
-    console.log("Modal closed");
-}
-
-function add() {
-    // Logic to handle adding the category  
-    console.log("Category added");
-}  
